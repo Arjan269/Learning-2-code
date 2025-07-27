f32670b34d4bf45a04ca915fd59b633b2e82a478
Arjan269 <Arjanp@missionreadyhq.com>
2025-07-28 11:18:37 +1200

    adding week02 day05

---
+++ week-02/Day-05-js-intro/script.js
@@ -0,0 +1,35 @@
+console.log("Hello World from script.js");
+
+let myFavColour;
+console.log(myFavColour);
+
+const myName = "Arjan"
+console.log(myName);
+
+const firstName = "Crocodile", lastName = "Dundee"
+console.log(firstName, lastName);
+
+const x = 5;
+const y = 8;
+const z = x + y;
+console.log ("z has a vaule of:", z);
+
+// ------ Excercise 4 ------ //
+const firstAndLastName = "Arjan Petera";
+let age = 28;
+
+// ------ Excercise 5 ------ //
+console.log(firstAndLastName, age);
+
+const ageCalc = 2025-1997;
+console.log(firstAndLastName, age, ageCalc);
+
+// ----- Excercise 6 ----- //
+let num = 5;
+num = 10; //Reassignment is allowed
+console.log(num); //10
+
+let favFood;
+
+favFood = "Lasagna";
+console.log(favFood);
