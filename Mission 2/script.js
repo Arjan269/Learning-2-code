79ce0cf8ed37b2af713ddd18a1d29407a8482b61
Arjan269 <Arjanp@missionreadyhq.com>
2025-08-11 08:01:58 +1200

    saving mission 2 before deleting parts an retrying

--- Mission 2/script.js
+++ Mission 2/script.js
@@ -1,1 +1,52 @@
 console.log(`Jarvis Initiate Connection`);
+
+const nextButton = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video');
+
+const movieList = ['video/luffy-vs-kaido.mp4'];
+
+let index = 0;
+nextButton.addEventListener('click', function(){
+
+    index += 1
+    video.src = movieList[index];
+
+    if (index === 1){
+        index = -1;
+    }
+});
+
+// initializeSlider();
+
+// const slides = document.querySelectorAll(".slides img");
+// let slideIndex = 0;
+// let intervalId = null;
+
+// function initializeSlider(){
+//     slides[slideIndex].classList.add("displaySlide");
+//     intervalId = setInterval(nxtSlide, 5000);
+    
+// }
+// function showSlide(index){
+
+//     if(index >= slides.length){
+//         slideIndex = 0;
+//     }
+//     else if(index < 0){
+//         slideIndex = slides.length - 1;
+//     }
+
+//     slides.forEach(slide => {
+//         slide.classList.remove("displaySlide");
+//     });
+//     slides[slideIndex].classList.add("displaySlide");
+// }
+// function prevSlide(){
+//     clearInterval(intervalId);
+//     slideIndex--;
+//     showSlide(slideIndex);
+// }
+// function nxtSlide(){
+//     slideIndex++;
+//     showSlide(slideIndex);
+// }
